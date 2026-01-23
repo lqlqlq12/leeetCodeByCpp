c088f959cb20775814cffbd0a4c138be9d897862
连衡权 <lianhengquan@cmos.chinamobile.com>
2026-01-23 19:34:52 +0800

    73

--- 1_500/67_88/73_setZeroes.cpp
+++ 1_500/67_88/73_setZeroes.cpp
@@ -3,9 +3,65 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size(), n = matrix[0].size();
+        bool isFirstRowZero = false, isFirstColZero = false;
+        for (int i = 0; i < n; i++)
+        {
+            if (matrix[0][i] == 0)
+            {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            if (matrix[i][0] == 0)
+            {
+                isFirstColZero = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (isFirstRowZero)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+        if (isFirstColZero)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
     }
 };
