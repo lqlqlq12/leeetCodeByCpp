7aee5f5a71a7775ec2962a4c0b290444eb0b05a3
连衡权 <lianhengquan@cmos.chinamobile.com>
2026-02-09 19:47:52 +0800

    87 88

--- 1_500/67_88/87_isScramble.cpp
+++ 1_500/67_88/87_isScramble.cpp
@@ -7,13 +7,62 @@
 在 x 和 y 这两个子字符串上继续从步骤 1 开始递归执行此算法。
 给你两个 长度相等 的字符串 s1 和 s2，判断 s2 是否是 s1 的扰乱字符串。如果是，返回 true ；否则，返回 false 。 */
 #include <string>
+#include <vector>
+
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-string target;
-    bool isScramble(string s1, string s2) {
-        
+    string s1, s2;
+    vector<int> dp;
+    int len;
+
+    int &at(int i, int j, int k)
+    {
+        return dp[i * len * (len + 1) + j * (len + 1) + k];
     }
-    
+
+    bool isScramble(string s1, string s2)
+    {
+        this->len = s1.size();
+        this->s1 = s1;
+        this->s2 = s2;
+        dp = vector<int>(len * len * (len + 1));
+        return dfs(0, 0, len);
+    }
+
+    bool dfs(int index1, int index2, int length)
+    {
+        if (at(index1, index2, length) != 0)
+        {
+            return at(index1, index2, length) == 1;
+        }
+        if (s1.substr(index1, length) == s2.substr(index2, length))
+        {
+            at(index1, index2, length) = 1;
+            return true;
+        }
+        vector<int> arr1(26), arr2(26);
+        for (int i = 0; i < length; i++)
+        {
+            arr1[s1[index1 + i] - 'a']++;
+            arr2[s2[index2 + i] - 'a']++;
+        }
+        if (arr1 != arr2)
+        {
+            at(index1, index2, length) = -1;
+            return false;
+        }
+        for (int i = 1; i < length; i++)
+        {
+            if ((dfs(index1, index2, i) && dfs(index1 + i, index2 + i, length - i)) || (dfs(index1, index2 + length - i - 1, i) && dfs(index1 + i, index2, length - i)))
+            {
+                at(index1, index2, length) = 1;
+                return true;
+            }
+        }
+        at(index1, index2, length) = -1;
+        return false;
+    }
 };
