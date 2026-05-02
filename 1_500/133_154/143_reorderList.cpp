6b54a356c0594d065598db270f853ea60ef66903
lianhengquan <lianhengquan@cmos.chinamobile.com>
2026-05-02 22:04:10 +0800

    143

--- 1_500/133_154/143_reorderList.cpp
+++ 1_500/133_154/143_reorderList.cpp
@@ -44,16 +44,3 @@ public:
         head->next = slow;
     }
 };
-
-int main() {
-    ListNode *l1 = new ListNode(1);
-    ListNode *l2 = new ListNode(2);
-    ListNode *l3 = new ListNode(3);
-    ListNode *l4 = new ListNode(4);
-    l1->next = l2;
-    l2->next = l3;
-    l3->next = l4;
-    Solution sol;
-    sol.reorderList(l1);
-    return 0;
-}
